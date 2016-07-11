831c9eb6938a9800bc2811a40dbf3b384206f94a
HeadwindFly <HeadwindFly@163.com>
2016-07-11 12:54:38 +0800

    Added more basic usages

--- examples/base.go
+++ examples/base.go
@@ -7,10 +7,13 @@ package main
 import (
 	"github.com/headwindfly/clevergo"
 	"log"
+	"os"
+	"path"
 )
 
 var (
 	helloCleverGo = []byte("Hello CleverGo!\n")
+	resourcesPath = path.Join(os.Getenv("GOPATH"), "src", "github.com", "headwindfly", "clevergo", "examples")
 )
 
 type User struct {
@@ -48,6 +51,17 @@ func xml(ctx *clevergo.Context) {
 	}, "")
 }
 
+func params(ctx *clevergo.Context) {
+	name := ctx.RouterParams.ByName("name")
+	ctx.Textf("Hello %s.", name)
+}
+
+func multiParams(ctx *clevergo.Context) {
+	param1 := ctx.RouterParams.ByName("param1")
+	param2 := ctx.RouterParams.ByName("param2")
+	ctx.Textf("Your params is %s and %s", param1, param2)
+}
+
 func main() {
 	// Create a router instance.
 	router := clevergo.NewRouter()
@@ -58,6 +72,14 @@ func main() {
 	router.GET("/json", clevergo.HandlerFunc(json))
 	router.GET("/jsonp", clevergo.HandlerFunc(jsonp))
 	router.GET("/xml", clevergo.HandlerFunc(xml))
+	// Navigate to http://127.0.0.1:8080/params/yourname.
+	router.GET("/params/:name", clevergo.HandlerFunc(params))
+	// Navigate to http://127.0.0.1:8080/multi-params/param1/param2.
+	router.GET("/multi-params/:param1/:param2", clevergo.HandlerFunc(multiParams))
+
+	// Static resource files.
+	// Navigate to http://127.0.0.1:8080/examples/base.go
+	router.ServeFiles("/examples/*filepath", resourcesPath)
 
 	// Start server.
 	log.Fatal(clevergo.ListenAndServe(":8080", router.Handler))
