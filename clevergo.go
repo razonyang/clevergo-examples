c0607f9b42d833d9ae1ba90c2a2979ba0a2dba8c
HeadwindFly <HeadwindFly@163.com>
2016-08-09 13:45:10 +0800

    Changed clevergo version and renamed Run() to info()

--- clevergo.go
+++ clevergo.go
@@ -12,7 +12,7 @@ import (
 )
 
 const (
-	Version = "1.0.0"
+	Version = "1.0.2"
 	Logo    = `  ____ _     _______     _______ ____   ____  ___
  / ___| |   | ____\ \   / / ____|  _ \ / ___|/ _ \
 | |   | |   |  _|  \ \ / /|  _| | |_) | |  _| | | |
@@ -20,26 +20,26 @@ const (
  \____|_____|_____|  \_/  |_____|_| \_\\____|\___/ `
 )
 
-func Run() {
+func info() {
 	fmt.Printf("\x1b[36;1m%s %s\x1b[0m\n\n\x1b[32;1mStarted at %s\x1b[0m\n", Logo, Version, time.Now())
 }
 
 func ListenAndServe(addr string, handler fasthttp.RequestHandler) error {
-	Run()
+	info()
 	return fasthttp.ListenAndServe(addr, handler)
 }
 
 func ListenAndServeUNIX(addr string, mode os.FileMode, handler fasthttp.RequestHandler) error {
-	Run()
+	info()
 	return fasthttp.ListenAndServeUNIX(addr, mode, handler)
 }
 
 func ListenAndServeTLS(addr, certFile, keyFile string, handler fasthttp.RequestHandler) error {
-	Run()
+	info()
 	return fasthttp.ListenAndServeTLS(addr, certFile, keyFile, handler)
 }
 
 func ListenAndServeTLSEmbed(addr string, certData, keyData []byte, handler fasthttp.RequestHandler) error {
-	Run()
+	info()
 	return fasthttp.ListenAndServeTLSEmbed(addr, certData, keyData, handler)
 }
